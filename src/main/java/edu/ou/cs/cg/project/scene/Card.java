6fc377cced270753df347d21e05760d8fade25a7
Parker Brandt <parker.brandt101@gmail.com>
2022-04-19 03:41:01 -0500

    Added some basic functionality for the card

--- src/main/java/edu/ou/cs/cg/project/scene/Card.java
+++ src/main/java/edu/ou/cs/cg/project/scene/Card.java
@@ -1,7 +1,13 @@
 package edu.ou.cs.cg.project.scene;
 
+import com.jogamp.opengl.GL2;
 import edu.ou.cs.cg.utilities.Node;
+import edu.ou.cs.cg.utilities.Point3D;
 
+import java.awt.*;
+import java.awt.geom.Point2D;
+import java.io.*;
+import java.util.ArrayList;
 import java.util.Random;
 
 /**
@@ -14,13 +20,34 @@ import java.util.Random;
 public class Card extends Node {
 
     //****************************************
+    // Private Class Members
+    //****************************************
+    private static final String TREE_IMG_FILE =     "images/tree.jpg";
+    private static final String TREE_TRUNK_FILE =   "images/trunk.jpg";
+
+    //****************************************
     // Private Variables
     //****************************************
-    private final int id;           // The unique ID of the card
+    private int     id;                 // The unique ID of the card
+    private Color   outColor;           // The color of the outside of the card
+    private Color   inColor;            // The color of the inside of the card
+
+    private boolean isOpen;             // Decide if the card is open or closed
+    private int rotateAngle;            // The amount the front face of card is opened
+
+    private Point3D loc;                // The x,y,z location of the bottom left of the card
+    private int width;                  // The width of each side of the card
+    private int height;                 // The height of each side of the card
+
+    private ArrayList<String>  text;     // All text on the card
+
+    private ArrayList<Point2D.Double> trees;    // The location of all trees on the inside of the card
+
+    private int time;       // The time of day on the card - ranges from 0 to 1800 (30 seconds) for day/night cycle
 
 
     //****************************************
-    // Private Variables
+    // Public Variables
     //****************************************
 
 
@@ -39,13 +66,42 @@ public class Card extends Node {
         // Initialize variables
         Random rand = new Random();
         this.id = rand.nextInt(1000);
+
+        outColor = new Color(255, 0, 0);
+        inColor = new Color(255, 230, 230);
+
+        // Initialize location and dimensions of the card
+
+
+        isOpen = false;
+        rotateAngle = 0;
+
+
+        // Add some default text
+        text.add("The Default Card");
+
+        // Add two trees to the inside
+
+
+        // Start the day/night cycle
+        time = 0;
     }
 
     /**
-     * Loads a card from a CSV file
+     * Loads a card from a CSV file and initializes data
      * @param filename the file location/name
      */
     public Card(String filename) {
+
+        // Initialize variables
+        time = 0;
+
+        // Load the data from a file
+        try {
+            load(filename);
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
@@ -55,12 +111,79 @@ public class Card extends Node {
     //****************************************
 
     @Override
-    
+    protected void change(GL2 gl) {
+
+    }
+
+    @Override
+    protected void depict(GL2 gl) {
+
+        // Update the day/night cycle
+        time++;
+
+        // Determine if card should be opened or not
+        if(isOpen) {
+            if(rotateAngle < 180)
+                rotateAngle += 5;
+        } else {
+            if(rotateAngle > 15)
+                rotateAngle -= 5;
+        }
+
+
+    }
 
 
     //****************************************
     // Private Methods
     //****************************************
+
+    // File save/load methods
+
+    /**
+     * Save the card data to a file
+     * @param filename
+     * @throws IOException
+     */
+    public void save(String filename) throws IOException {
+
+        BufferedWriter writer = new BufferedWriter(new FileWriter(filename));
+    }
+
+    /**
+     * Read the card data from the specified filename
+     * @param filename
+     * @throws IOException
+     */
+    public void load(String filename) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new FileReader(filename));
+        String line = "";
+        while((line = reader.readLine()) != null) {
+
+        }
+    }
+
+
+    //****************************************
+    // Getters and Setters
+    //****************************************
+
+    // Getters
+    public Color getOutColor() {
+        return outColor;
+    }
+
+    public Color getInColor() {
+        return inColor;
+    }
+
+    public Point3D getLoc() {
+        return loc;
+    }
+
+
+    // Setters
 
 
 }
