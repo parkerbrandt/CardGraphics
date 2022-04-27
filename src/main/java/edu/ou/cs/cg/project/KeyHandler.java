314a6a3a7174e3e5a32d5592fb8fbd16a0d92acd
Parker Brandt <parker.brandt101@gmail.com>
2022-04-27 18:58:18 -0500

    Added basic outline for all needed keys at the moment

--- src/main/java/edu/ou/cs/cg/project/KeyHandler.java
+++ src/main/java/edu/ou/cs/cg/project/KeyHandler.java
@@ -55,8 +55,35 @@ public class KeyHandler extends KeyAdapter {
             case KeyEvent.VK_1:
                 break;
 
-            // Enable edit mode
-            case KeyEvent.VK_E:
+            case KeyEvent.VK_2:
+                break;
+
+            case KeyEvent.VK_3:
+                break;
+
+            case KeyEvent.VK_4:
+                break;
+
+            case KeyEvent.VK_5:
+                break;
+
+            case KeyEvent.VK_6:
+                break;
+
+            case KeyEvent.VK_7:
+                break;
+
+            case KeyEvent.VK_8:
+                break;
+
+            case KeyEvent.VK_9:
+                break;
+
+            // Move the selected tree to the left
+            case KeyEvent.VK_A:
+                if(model.isEditMode()) {
+
+                }
                 break;
 
             // If in edit mode, shift through some predetermined colors for outside of the card
@@ -64,13 +91,43 @@ public class KeyHandler extends KeyAdapter {
                 model.switchColor();
                 break;
 
-            // Save card to the next open slot, if none available delete #1
-            case KeyEvent.VK_S:
+            // Delete the current card and set to be default
+            // If in edit mode, move the tree to the right
+            case KeyEvent.VK_D:
+                if(model.isEditMode()) {
+
+                } else {
+
+                }
                 break;
 
-            // Delete the current card and set to be default
-            case KeyEvent.VK_D:
-            case KeyEvent.VK_DELETE:
+            // Enable edit mode
+            case KeyEvent.VK_E:
+                model.setEditMode(!model.isEditMode());
+                break;
+
+            // Save card to the next open slot, if none available delete #1
+            // If edit mode is enabled, move the tree
+            case KeyEvent.VK_S:
+                if(model.isEditMode()) {
+
+                } else {
+
+                }
+                break;
+
+            // Edit text if in edit mode
+            case KeyEvent.VK_T:
+                if(model.isEditMode()) {
+                    model.changeFrontText();
+                }
+                break;
+
+            // Move the selected tree up
+            case KeyEvent.VK_W:
+                if(model.isEditMode()) {
+
+                }
                 break;
 
             // Open/Close the card
@@ -81,6 +138,11 @@ public class KeyHandler extends KeyAdapter {
             // Open/Close the instructions
             case KeyEvent.VK_ESCAPE:
                 model.setShowInstructions(!model.showInstructions());
+                break;
+
+            // Loop through each tree on the card and allow the user to move the
+            case KeyEvent.VK_RIGHT:
+
                 break;
 
             default:
